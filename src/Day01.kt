1cc4c45304c05186371efef4f50ead8091d065b8
Craig Martin <me@craigmart.in>
2021-12-01 17:31:11 -0600

    Solve day01 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,22 +1,32 @@
 fun main(args: Array<String>) {
-    fun part1(input: List<String>): Int {
-        val convertedInput = input.map { it.toInt() }
+    fun part1(input: List<Int>): Int {
         var count = 0
-        for (i in 1 until convertedInput.size)
-            if (convertedInput[i] > convertedInput[i - 1])
+        for (i in 1 until input.size)
+            if (input[i] > input[i - 1])
                 count++
         return count
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        var count = 0
+        for (i in 0..input.size - 4) {
+            val windowA = input.drop(i).take(3)
+            val windowB = input.drop(i+1).take(3)
+            //println("Window A: $windowA -> ${windowA.sum()};  Window B: $windowB -> ${windowB.sum()}")
+
+            if (windowA.sum() < windowB.sum())
+                count++
+        }
+
+        return count
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInput("Day01_test").map { it.toInt() }
     check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
