402f4041dccb95ff1dc39b02ef746b2008430256
Craig Martin <me@craigmart.in>
2021-12-02 08:12:07 -0600

    Solve day 2 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,25 @@
 fun main(args: Array<String>) {
     fun part1(input: List<String>): Int {
-        return input.size
+        var horizontalPosition = 0
+        var depth = 0
+
+        var commands = input.map {
+            val split = it.split(' ')
+            Pair(split[0], split[1].toInt())
+        }
+
+        for (command in commands) {
+            //println("${command.first} ${command.second}")
+            when (command.first) {
+                "forward" -> horizontalPosition += command.second
+                "down" -> depth += command.second
+                "up" -> depth -= command.second
+            }
+        }
+
+        //println("Horizontal: $horizontalPosition; Depth: $depth")
+
+        return horizontalPosition * depth
     }
 
     fun part2(input: List<String>): Int {
@@ -9,8 +28,8 @@ fun main(args: Array<String>) {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 1)
-    check(part2(testInput) == 1)
+    check(part1(testInput) == 150)
+    //check(part2(testInput) == 1)
 
     val input = readInput("Day02")
     println(part1(input))
