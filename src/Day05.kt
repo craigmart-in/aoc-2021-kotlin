729c2c8a2d5e2602ee4678775cf7f171d005bcc7
Craig Martin <me@craigmart.in>
2021-12-05 13:53:50 -0600

    Solve day 5 part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,8 +1,9 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        val ventCounts: MutableMap<Point, Int> = mutableMapOf()
+import kotlin.math.abs
+import kotlin.math.max
 
-        input.forEach { it ->
+fun main() {
+    fun parseInput(input: List<String>): List<Pair<Point, Point>> {
+        return input.map { it ->
             val coordinates = it.split(" -> ")
             var points = coordinates[0].split(",").map { it.toInt() }
 
@@ -12,38 +13,44 @@ fun main() {
 
             val point2 = Point(points[0], points[1])
 
+            Pair(point1, point2)
+        }
+    }
+
+    fun getVentCount(point1: Point, point2: Point): Int {
+        return max(abs(point1.x - point2.x), abs(point1.y - point2.y))
+    }
+
+    fun getChange(start: Int, end: Int): Int {
+        return end.compareTo(start)
+    }
+
+    fun getDangerousVentCount(input: List<String>, ignoreDiagonal: Boolean): Int {
+        val ventCounts: MutableMap<Point, Int> = mutableMapOf()
+        val coordinates = parseInput(input)
+
+        for(coordinate in coordinates) {
+            val point1 = coordinate.first
+            val point2 = coordinate.second
+
             val ventLocations: MutableList<Point> = mutableListOf()
 
-            //print("$it: ")
-            if (point1.x == point2.x) {
-                //println("Horizontal")
-                if (point1.y < point2.y) {
-                    for (index in point1.y..point2.y) {
-                        ventLocations.add(Point(point1.x, index))
-                    }
-                }
-                else {
-                    for (index in point2.y..point1.y) {
-                        ventLocations.add(Point(point1.x, index))
-                    }
-                }
-            }
-            else if (point1.y == point2.y) {
-                //println("Vertical")
-                if (point1.x < point2.x) {
-                    for (index in point1.x..point2.x) {
-                        ventLocations.add(Point(index, point1.y))
-                    }
-                }
-                else {
-                    for (index in point2.x..point1.x) {
-                        ventLocations.add(Point(index, point1.y))
-                    }
-                }
+            var count = getVentCount(point1, point2)
+            var dx = getChange(point1.x, point2.x)
+            var dy = getChange(point1.y, point2.y)
+
+            //print("(${point1.x}, ${point1.y}) -> (${point2.x}, ${point2.y}); Count: $count; dx: $dx; dy: $dy;  ")
+
+            if (ignoreDiagonal && dx != 0 && dy != 0) {
+                //println("Skipping because of diagonal")
+                continue
             }
 
-            //print(line)
-            //println()
+            for (i in 0..count) {
+                ventLocations.add(Point(point1.x + (dx * i), point1.y + (dy * i)))
+            }
+
+            //println(ventLocations)
 
             ventLocations.forEach { vent ->
                 if (ventCounts.contains(vent)) {
@@ -54,17 +61,22 @@ fun main() {
                 }
             }
         }
-        //println(diagram)
+
         return ventCounts.count { it.value > 1 }
     }
 
+    fun part1(input: List<String>): Int {
+        return getDangerousVentCount(input, true)
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        return getDangerousVentCount(input, false)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
     check(part1(testInput) == 5)
+    check(part2(testInput) == 12)
 
     val input = readInput("Day05")
     println(part1(input))
