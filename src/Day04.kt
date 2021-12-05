a4446d9526b87335380ed24067328823ca75de4d
Craig Martin <me@craigmart.in>
2021-12-05 12:00:48 -0600

    Solve day 4 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -13,11 +13,13 @@ fun main() {
         listOf(4,9,14,19,24)
     )
 
-    fun part1(input: List<String>): Int {
+    fun getDrawnNumbers(input: List<String>): List<Int> {
         val drawnNumbers = input[0].split(",").map { it.toInt() }
         //println("Drawn Numbers: $drawnNumbers")
+        return drawnNumbers
+    }
 
-        // Parse all boards
+    fun getBoards(input: List<String>): MutableList<List<Int>> {
         val boards: MutableList<List<Int>> = mutableListOf()
         for (row in 2..input.size step 6) {
             //println("Board:")
@@ -30,9 +32,19 @@ fun main() {
 
             boards.add(board)
         }
-
         //boards.forEachIndexed { index, list ->  println("Board $index: $list")}
+        return boards
+    }
 
+    fun calculateScore(board: List<Int>, winningIndexes: MutableList<Int>, lastWinningNumber: Int): Int {
+        val losingNumbers = board.filterIndexed { index, s -> !winningIndexes.contains(index) }
+
+        //println ("Losing Numbers: $losingNumbers; Sum: ${losingNumbers.sum()}")
+
+        return losingNumbers.sum() * lastWinningNumber
+    }
+
+    fun part1(drawnNumbers: List<Int>, boards: MutableList<List<Int>>): Int {
         val winningIndexesByBoard = List<MutableList<Int>>(boards.size) { mutableListOf() }
         var lastWinningNumber = -1
         var winningBoardIndex = -1
@@ -60,22 +72,51 @@ fun main() {
 
         //println("Winning Board: $winningBoardIndex; Last Winning Number: $lastWinningNumber")
 
-        val losingNumbers = boards[winningBoardIndex].filterIndexed { index, s -> !winningIndexesByBoard[winningBoardIndex].contains(index) }
-
-        //println ("Losing Numbers: $losingNumbers; Sum: ${losingNumbers.sum()}")
-
-        return losingNumbers.sum() * lastWinningNumber
+        return calculateScore(boards[winningBoardIndex], winningIndexesByBoard[winningBoardIndex], lastWinningNumber)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(drawnNumbers: List<Int>, boards: MutableList<List<Int>>): Int {
+        val winningIndexesByBoard = List<MutableList<Int>>(boards.size) { mutableListOf() }
+        val winningBoardIndexes = mutableListOf<Int>()
+        var lastWinningNumber = -1
+
+        // Loop through all the drawn numbers
+        // Then loop through all the boards
+        loop@ for (drawnNumber in drawnNumbers) {
+            for ((index, board) in boards.withIndex()) {
+                // Board already won so ignore it
+                if (winningBoardIndexes.contains(index)) continue
+
+                // Check if the board contains that drawn number
+                val winningIndex = board.indexOf(drawnNumber)
+                if (winningIndex == -1) continue
+
+                winningIndexesByBoard[index].add(winningIndex)
+
+                // Check if the board is a winner by seeing if the winning indexes matches any of the winning conditions
+                for (winningIndexes in winningNumberIndexes) {
+                    if (winningIndexesByBoard[index].containsAll(winningIndexes)) {
+                        lastWinningNumber = drawnNumber
+                        winningBoardIndexes.add(index)
+                    }
+                }
+            }
+        }
+
+        //println("Winning Board: $winningBoardIndex; Last Winning Number: $lastWinningNumber")
+
+        return calculateScore(boards[winningBoardIndexes.last()], winningIndexesByBoard[winningBoardIndexes.last()], lastWinningNumber)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
-    check(part1(testInput) == 4512)
+    var drawnNumbers = getDrawnNumbers(testInput)
+    var boards = getBoards(testInput)
+    check(part1(drawnNumbers, boards) == 4512)
 
     val input = readInput("Day04")
-    println(part1(input))
-    println(part2(input))
+    drawnNumbers = getDrawnNumbers(input)
+    boards = getBoards(input)
+    println(part1(drawnNumbers, boards))
+    println(part2(drawnNumbers, boards))
 }
