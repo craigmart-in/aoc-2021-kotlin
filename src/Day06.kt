b131ecdbe56a62815826015bd47aef65c31d22c2
Craig Martin <me@craigmart.in>
2021-12-07 23:13:35 -0600

    Solve day 6 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -3,35 +3,53 @@ fun main() {
         return input.split(",").map { it.toInt() }.toMutableList()
     }
 
-    fun part1(fishes: MutableList<Int>): Int {
-        println("Initial state: $fishes")
-        for (day in 0..79) {
-            val fishCount = fishes.size - 1
-            for (index in 0..fishCount) {
-                if (fishes[index] == 0) {
-                    fishes[index] = 6
-                    fishes.add(8)
+    fun countFishes(fishes: MutableList<Int>, days: Int): Long {
+        var fishAgeByCount = fishes
+            .groupBy { it }
+            .mapValues { it.value.size.toLong() }
+            .toSortedMap()
+
+        println("Initial state: $fishAgeByCount")
+
+        for (day in 0..days) {
+            val temp = mutableMapOf<Int, Long>().toSortedMap()
+            for (age in 8 downTo 0) {
+                if (age == 0) {
+                    temp[8] = fishAgeByCount.getOrDefault(age, 0)
+                    temp[6] = temp.getOrDefault(6, 0) + fishAgeByCount.getOrDefault(age, 0)
                 }
                 else {
-                    fishes[index] = fishes[index] - 1
+                    temp[age - 1] = fishAgeByCount.getOrDefault(age, 0)
                 }
             }
 
-            println("After day ${day + 1}: $fishes")
+            fishAgeByCount = temp
+            println("After day ${day + 1}: $fishAgeByCount")
         }
 
-        return fishes.size
+        var count = 0L
+        fishAgeByCount.forEach{
+            count += it.value
+        }
+
+        println("Sum: $count")
+
+        return count
+    }
+
+    fun part1(fishes: MutableList<Int>): Long {
+        return countFishes(fishes, 79)
     }
 
     fun part2(fishes: MutableList<Int>): Long {
-        return 1
+        return countFishes(fishes, 255)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
     val parsedTestInput = getInitialLanternFish(testInput[0])
-    check(part1(parsedTestInput) == 5934)
-    
+    check(part1(parsedTestInput) == 5934L)
+    check(part2(parsedTestInput) == 26984457539)
 
     val input = readInput("Day06")
     val parsedInput = getInitialLanternFish(input[0])
